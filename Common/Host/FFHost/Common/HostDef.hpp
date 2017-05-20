bc2ae2dad17e1443277acd26b938e88b00b01811
Owl <owlgame@163.com>
2017-05-20 18:03:33 +0800

    基于组件系统的客户端结构

---
+++ Common/Host/FFHost/Common/HostDef.hpp
@@ -0,0 +1,15 @@
+/*
+* FILE: Entity.h
+*
+* DESCRIPTION: Scene object base class
+*
+* CREATED BY: Owl Yang, 2017/5/20
+*
+* HISTORY:
+*
+* Copyright (t) 2016 FF Studio, All Rights Reserved.
+*/
+
+#pragma once
+
+using CompSign_t = int;
---
+++ Common/Host/FFHost/Common/MemMonitored.hpp
@@ -0,0 +1,17 @@
+/*
+* FILE: MemMonitored.hpp
+*
+* DESCRIPTION:
+*
+* CREATED BY: Owl Yang, 2017/5/20
+*
+* HISTORY:
+*
+* Copyright (t) 2016 FF Studio, All Rights Reserved.
+*/
+
+class FFMemMonitored
+{
+public:
+	operator new(){}
+};
